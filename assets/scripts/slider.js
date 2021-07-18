38f2a23076f23124fb8cef674010c408a4d4e4fa
natalliasdh <natallia.sdh@gmail.com>
2021-07-18 10:39:34 -0400

    Interactive pricing added

--- assets/scripts/slider.js
+++ assets/scripts/slider.js
@@ -1,31 +1,45 @@
 let slider = document.getElementById("priceslider");
+let monthSlider = document.getElementById("monthslider");
 let pageViews = document.getElementById("pageviews");
 let priceNumber = document.getElementById("pricenumber");
 
-slider.addEventListener('change', () => {
+
+function pageChange() {
 
     switch (slider.value) {
         case "1":
             pageViews.textContent = "10K";
-            priceNumber.textContent = "8";
+
+            priceCounter(8);
             break;
         case "3":
             pageViews.textContent = "50K";
-            priceNumber.textContent = "12";
+            priceCounter(12);
             break;
         case "5":
             pageViews.textContent = "100K";
-            priceNumber.textContent = "16";
+            priceCounter(16);
             break;
         case "7":
             pageViews.textContent = "500K";
-            priceNumber.textContent = "24";
+            priceCounter(24);
             break;
         case "9":
             pageViews.textContent = "1M";
-            priceNumber.textContent = "36";
+            priceCounter(36);
             break;
     }
 
 
-});
+
+
+
+};
+
+function priceCounter(price) {
+    if (monthSlider.value == "2") {
+        priceNumber.textContent = price;
+    } else {
+        priceNumber.textContent = (price * 0.85).toFixed(0);
+    }
+}
